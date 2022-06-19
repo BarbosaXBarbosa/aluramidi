344d8fbbb64c5deac6225f914482951c88bf678f
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-06-19 17:31:22 -0300

    Criando rotinas para puxar o som de cada tecla de acordo com a tecla clicada e limpando o código.

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 
 i=0;
 
 //enquanto
-while(i < listaDeTeclas.length ) {
-    listaDeTeclas[i].onclick = tocaSomPom;
+while (i < listaDeTeclas.length ) {
+
+
+    const tecla = listaDeTeclas[i]
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    } ;
     i ++;
 
     console.log(i);
