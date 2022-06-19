7ea35199b0df807a72c20cecd70e01635c293664
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-06-19 19:51:19 -0300

    Substituindo o while.

--- main.js
+++ main.js
@@ -6,34 +6,16 @@ function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-
-i=0;
-
 //enquanto
-while (i < listaDeTeclas.length ) {
+for (i=0; i < listaDeTeclas.length; i++ ) {
 
-
-    const tecla = listaDeTeclas[i]
-
+    const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
-
-    console.log(idAudio);
 
     tecla.onclick = function() {
         tocaSom(idAudio);
-    } ;
-    i ++;
+    }
 
-    console.log(i);
 }
 
-
-
-/*function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-*/
