730a1f8d6dd896ad1f10f4bb8665a1af10c08f9d
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-06-20 13:37:31 -0300

    Concluindo um tratamento de erros com operadores lógicos.

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
-
-    if(elemento ===null) {
-        console.log('Elemento não encontrado');
-    } else {
-        if(elemento.localName === 'audio'){
-            elemento.play();  
-        } else {
-            console.log('Elemento inválido');
-        }
+ 
+    if(elemento != null && elemento.localName === 'audio') {
+        elemento.play();  
+    }else {
+        console.log('Elemento não encontrado ou seletor inválido.');
     }
+    
 }
 
 //para
